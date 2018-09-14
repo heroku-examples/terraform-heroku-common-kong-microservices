aae75c6f2959d6cf756370d1abedeb7aa97780e7
Mars Hall <mars@heroku.com>
2018-09-14 14:19:55 -0700

    Convert to Common Runtime

--- main.tf
+++ main.tf
@@ -2,36 +2,19 @@ variable "name" {
   type = "string"
 }
 
-variable "dns_zone" {
-  type        = "string"
-  description = "DNS zone for new records (must already be setup in the DNSimple account)"
-}
-
-variable "heroku_enterprise_team" {
+variable "heroku_team" {
   type = "string"
 }
 
-variable "heroku_private_region" {
+variable "heroku_region" {
   type    = "string"
-  default = "oregon"
-}
-
-variable "hello_world_header_message" {
-  type        = "string"
-  description = "Custom message to output in heroku-kong's 'Hello-World' HTTP header."
-  default     = "🌞💎"
+  default = "us"
 }
 
 locals {
-  kong_app_name           = "${var.name}-proxy"
-  kong_base_url           = "https://${local.kong_app_name}.${var.dns_zone}"
-  kong_insecure_base_url  = "http://${local.kong_app_name}.herokuapp.com"
-  kong_admin_uri          = "${local.kong_base_url}/kong-admin"
-  kong_insecure_admin_uri = "${local.kong_insecure_base_url}/kong-admin"
-}
-
-provider "dnsimple" {
-  version = "~> 0.1"
+  kong_app_name  = "${var.name}-proxy"
+  kong_base_url  = "https://${local.kong_app_name}.herokuapp.com"
+  kong_admin_uri = "${local.kong_base_url}/kong-admin"
 }
 
 provider "heroku" {
@@ -44,7 +27,8 @@ provider "kong" {
   # Optional: use insecure until DNS is ready at dnsimple
   # kong_admin_uri = "${local.kong_insecure_admin_uri}"
   kong_admin_uri = "${local.kong_admin_uri}"
-  kong_api_key   = "${random_id.kong_admin_api_key.b64_url}"
+
+  kong_api_key = "${random_id.kong_admin_api_key.b64_url}"
 }
 
 provider "random" {
@@ -55,50 +39,25 @@ resource "random_id" "kong_admin_api_key" {
   byte_length = 32
 }
 
-# Private Space
-
-resource "heroku_space" "default" {
-  name         = "${var.name}"
-  organization = "${var.heroku_enterprise_team}"
-  region       = "${var.heroku_private_region}"
-}
-
 # Proxy app
 
 resource "heroku_app" "kong" {
-  name  = "${local.kong_app_name}"
-  space = "${heroku_space.default.name}"
-  acm   = true
+  name   = "${local.kong_app_name}"
+  acm    = true
+  region = "${var.heroku_region}"
 
   config_vars {
     KONG_HEROKU_ADMIN_KEY = "${random_id.kong_admin_api_key.b64_url}"
-    HELLO_WORLD_MESSAGE   = "${var.hello_world_header_message}"
   }
 
-  organization = {
-    name = "${var.heroku_enterprise_team}"
+  organization {
+    name = "${var.heroku_team}"
   }
-
-  region = "${var.heroku_private_region}"
-}
-
-resource "heroku_domain" "kong" {
-  app        = "${heroku_app.kong.id}"
-  hostname   = "${heroku_app.kong.name}.${var.dns_zone}"
-  depends_on = ["heroku_app_release.kong"]
-}
-
-resource "dnsimple_record" "kong" {
-  domain = "${var.dns_zone}"
-  name   = "${heroku_app.kong.name}"
-  value  = "${heroku_domain.kong.cname}"
-  type   = "CNAME"
-  ttl    = 30
 }
 
 resource "heroku_addon" "kong_pg" {
   app  = "${heroku_app.kong.id}"
-  plan = "heroku-postgresql:private-0"
+  plan = "heroku-postgresql:hobby-dev"
 }
 
 resource "heroku_slug" "kong" {
@@ -123,34 +82,38 @@ resource "heroku_formation" "kong" {
   app        = "${heroku_app.kong.id}"
   type       = "web"
   quantity   = 1
-  size       = "Private-S"
-  depends_on = ["heroku_app_release.kong", "dnsimple_record.kong"]
+  size       = "Standard-1x"
+  depends_on = ["heroku_app_release.kong"]
 
   provisioner "local-exec" {
-    # Optional: use insecure until DNS is ready at dnsimple
-    # command = "./bin/kong-health-check ${local.kong_insecure_base_url}/kong-admin"
     command = "./bin/kong-health-check ${local.kong_base_url}/kong-admin"
   }
 }
 
-# Internal app w/ proxy config
+# Microservice app w/ proxy config
+
+resource "random_id" "wasabi_internal_api_key" {
+  byte_length = 32
+}
 
 resource "heroku_app" "wasabi" {
-  name             = "${var.name}-wasabi"
-  space            = "${heroku_space.default.name}"
-  internal_routing = true
+  name   = "${var.name}-wasabi"
+  acm    = true
+  region = "${var.heroku_region}"
 
-  organization = {
-    name = "${var.heroku_enterprise_team}"
+  config_vars {
+    INTERNAL_API_KEY = "${random_id.wasabi_internal_api_key.b64_url}"
   }
 
-  region = "${var.heroku_private_region}"
+  organization {
+    name = "${var.heroku_team}"
+  }
 }
 
 resource "heroku_slug" "wasabi" {
   app                            = "${heroku_app.wasabi.id}"
   buildpack_provided_description = "Node.js"
-  file_path                      = "slugs/wasabi.tgz"
+  file_path                      = "slugs/wasabi-secure.tgz"
 
   process_types = {
     web = "npm start"
@@ -166,47 +129,34 @@ resource "heroku_formation" "wasabi" {
   app        = "${heroku_app.wasabi.id}"
   type       = "web"
   quantity   = 1
-  size       = "Private-S"
+  size       = "Standard-1x"
   depends_on = ["heroku_app_release.wasabi"]
 }
 
 resource "kong_service" "wasabi" {
   name       = "wasabi"
-  protocol   = "http"
+  protocol   = "https"
   host       = "${heroku_app.wasabi.name}.herokuapp.com"
-  port       = 80
+  port       = 443
   depends_on = ["heroku_formation.kong"]
 }
 
 resource "kong_route" "wasabi_hostname" {
   protocols  = ["https"]
-  hosts      = [ "${heroku_app.wasabi.name}.${var.dns_zone}" ]
+  paths      = ["/wasabi"]
   strip_path = true
   service_id = "${kong_service.wasabi.id}"
 }
 
-resource "heroku_domain" "wasabi" {
-  # The internal app's public DNS name is created on the Kong proxy.
-  app        = "${heroku_app.kong.id}"
-  hostname   = "${heroku_app.wasabi.name}.${var.dns_zone}"
-  depends_on = ["heroku_app_release.kong"]
-}
-
-resource "dnsimple_record" "wasabi" {
-  domain = "${var.dns_zone}"
-  name   = "${heroku_app.wasabi.name}"
-  value  = "${heroku_domain.wasabi.cname}"
-  type   = "CNAME"
-  ttl    = 30
-}
-
-resource "kong_plugin" "wasabi_hello_world" {
-  name     = "hello-world-header"
+resource "kong_plugin" "wasabi_internal_api_key" {
+  name       = "request-transformer"
   service_id = "${kong_service.wasabi.id}"
+
+  config = {
+    add.headers = "X-Internal-API-Key: ${random_id.wasabi_internal_api_key.b64_url}"
+  }
 }
 
 output "wasabi_service_url" {
-  # Optional: use insecure until DNS is ready at dnsimple
-  # value = "${local.kong_insecure_base_url}/wasabi"
-  value = "https://${heroku_app.wasabi.name}.${var.dns_zone}"
+  value = "${local.kong_base_url}/wasabi"
 }
